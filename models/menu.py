05283c0068d04c07fce8c3c2f3dad70f958a8eb5
Alex <alex@efswdev.ru>
2022-03-23 15:51:05 +0300

    - unused import statement

--- models/menu.py
+++ models/menu.py
@@ -1,6 +1,5 @@
 from typing import Iterable, Any
 
-from django import template
 from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _lazy
