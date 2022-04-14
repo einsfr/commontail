b4d4a68301f32a9b3c1590dcb99ca814c1506f20
Alex <alex@efswdev.ru>
2022-04-14 19:04:02 +0300

    + body block

--- models/page.py
+++ models/page.py
@@ -1,10 +1,17 @@
+import importlib
+
+from collections import OrderedDict, ValuesView
 from typing import List, Tuple, Union, Any, Type, Optional, Dict
 
 from django.conf import settings
 from django.core.paginator import Paginator, Page
 from django.db import models
+from django.utils.translation import gettext_lazy as _lazy
 
+from wagtail.admin.edit_handlers import StreamFieldPanel
 from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.search import index
 
 from .opengraph import AbstractOpenGraphAwarePage
 from .pagination import AbstractPaginationAwarePage, PaginatorPaginationData
@@ -12,7 +19,16 @@ from .seo import AbstractSEOAwarePage
 from .structureddata import AbstractStructuredDataAwarePage
 
 
-__all__ = ['AbstractBasePage', 'AbstractBaseIndexPage', 'AbstractContentStreamPage', ]
+__all__ = ['AbstractBasePage', 'AbstractBaseIndexPage', 'AbstractContentStreamPage',
+           'get_content_stream_page_body_block', ]
+
+
+def get_content_stream_page_body_block(**kwargs):
+    import_parts: List[str] = settings.COMMONTAIL_CONTENT_STREAM_PAGE_BODY_BLOCK.split('.')
+    module_name: str = '.'.join(import_parts[:-1])
+    body_block = getattr(importlib.import_module(module_name), import_parts[-1])
+
+    return body_block(**kwargs)
 
 
 class AbstractBasePage(AbstractOpenGraphAwarePage, AbstractStructuredDataAwarePage, AbstractSEOAwarePage):
@@ -73,3 +89,80 @@ class AbstractContentStreamPage(AbstractBasePage):
 
     class Meta:
         abstract = True
+
+    body = StreamField(
+        get_content_stream_page_body_block(required=False),
+        blank=True,
+        verbose_name=_lazy('page body'),
+    )
+
+    content_panels = [
+        StreamFieldPanel('body'),
+    ]
+
+    include_toc: bool = False
+
+    search_fields = AbstractBasePage.search_fields + [
+        index.SearchField('body', boost=1, partial_match=False),
+    ]
+
+    def build_table_of_contents(self) -> Tuple[ValuesView, dict]:
+        toc: OrderedDict = OrderedDict()
+        headings: dict = dict()
+        parent_h2: Optional[int] = None
+        parent_h3: Optional[int] = None
+        hid: int = 1
+
+        for hblock in [
+            block for block in self.body
+            if block.block_type == 'heading' and block.value['size'] in ['h2', 'h3', 'h4']
+        ]:
+            if hblock.value['size'] == 'h2':
+                toc[hid] = (hid, hblock, [])
+                headings[hblock.id] = hid
+                parent_h2 = hid
+                hid += 1
+            elif hblock.value['size'] == 'h3':
+                if not parent_h2:
+                    toc[hid] = (hid, None, [])
+                    parent_h2 = hid
+                    hid += 1
+                toc[parent_h2][2].append((hid, hblock, []))
+                headings[hblock.id] = hid
+                parent_h3 = hid
+                hid += 1
+            else:
+                if not parent_h3:
+                    if not parent_h2:
+                        toc[hid] = (hid, None, [])
+                        parent_h2 = hid
+                        hid += 1
+                    toc[parent_h2][2].append((hid, None, []))
+                    parent_h3 = hid
+                    hid += 1
+                for h3 in toc[parent_h2][2]:
+                    if h3[0] == parent_h3:
+                        h3[2].append((hid, hblock, []))
+                        headings[hblock.id] = hid
+                        hid += 1
+
+                        break
+
+        return toc.values(), headings
+
+    def get_context(self, request, *args, **kwargs):
+        if self.include_toc:
+            toc, headings = self.build_table_of_contents()
+        else:
+            toc = None
+            headings = None
+
+        context = super().get_context(request, *args, **kwargs)
+        context.update({
+            'table_of_contents': toc,
+            'headings': headings,
+        })
+
+        return context
+
+
