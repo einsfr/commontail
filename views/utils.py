1e2210f7de91759f0d85cff4edf4578be99fe276
Alex <alex@efswdev.ru>
2022-08-26 11:24:17 +0300

    check if favicon file exists

--- views/utils.py
+++ views/utils.py
@@ -1,11 +1,19 @@
+import os.path
+from typing import Union
+
 from django.conf import settings
 from django.contrib.staticfiles import finders
 from django.http.request import HttpRequest
-from django.http.response import FileResponse
+from django.http.response import FileResponse, HttpResponseNotFound
 
 
 __all__ = ['favicon', ]
 
 
-def favicon(request: HttpRequest) -> FileResponse:
-    return FileResponse(open(finders.find(settings.COMMONTAIL_FAVICON_ICO_STATIC_PATH), 'rb'))
+def favicon(request: HttpRequest) -> Union[FileResponse, HttpResponseNotFound]:
+    path = finders.find(settings.COMMONTAIL_FAVICON_ICO_STATIC_PATH)
+
+    if path and os.path.exists(path):
+        return FileResponse(open(path, 'rb'))
+    else:
+        return HttpResponseNotFound()
